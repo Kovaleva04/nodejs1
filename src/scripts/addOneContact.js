d00ec7a6d4e6fa9499175fb47a8c2610fafacd86
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-09-09 08:29:41 +0300

    .

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,34 +1,7 @@
-import { PATH_DB } from '../constants/contacts.js';
-import fs from 'node:fs/promises';
-import { createFakeContact } from '../utils/createFakeContact.js';
+import { generateContacts } from "./generateContacts.js";
 
 export const addOneContact = async () => {
-  let existingData = [];
-
-  try {
-    const fileData = await fs.readFile(PATH_DB, 'utf8');
-    existingData = JSON.parse(fileData);
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      console.error('Помилка читання файлу:', err);
-      return;
-    }
-  }
-
-  try {
-    const contact = createFakeContact();
-    existingData.push(contact);
-  } catch (err) {
-    console.error('Помилка при створенні контакту:', err);
-    return;
-  }
-
-  try {
-    await fs.writeFile(PATH_DB, JSON.stringify(existingData, null, 2));
-    console.log('Дані успішно записані у файл.');
-  } catch (err) {
-    console.error('Помилка запису у файл:', err);
-  }
+   generateContacts(1);
 };
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,35 +1,18 @@
-import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from "../constants/contacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
 import fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {
-  let existingData = [];
+export const generateContacts = async (number) => {
+    const fetchContacts = await readContacts();
 
-  try {
-    const fileData = await fs.readFile(PATH_DB, 'utf8');
-    existingData = JSON.parse(fileData);
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      console.error('Помилка читання файлу:', err);
-      return;
+    for(let i = 0; i < number; i++) {
+        fetchContacts.push(createFakeContact());
     }
-  }
 
-  for (let i = 0; i < number; i++) {
-    try {
-      const contact = createFakeContact();
-      existingData.push(contact);
-    } catch (err) {
-      console.error('Помилка при створенні контакту:', err);
-    }
-  }
-
-  try {
-    await fs.writeFile(PATH_DB, JSON.stringify(existingData, null, 2));
-    console.log('Дані успішно записані у файл.');
-  } catch (err) {
-    console.error('Помилка запису у файл:', err);
-  }
+    await fs.writeFile(PATH_DB, JSON.stringify(fetchContacts), 'utf-8');
 };
 
-generateContacts(5);
+if (process.argv[1].includes('generateContacts.js') === true) {
+    generateContacts(5);
+}
